7af185b11a8f40b2d7f0905645232fe81e2b15a4
Devon Walker <dejvonwalker@gmail.com>
2022-10-26 01:01:04 -0400

    First Commit

---
+++ Assets/Scripts/Messages.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Messages : MonoBehaviour
+{
+    public TextMeshProUGUI firstMission;
+    public TextMeshProUGUI secondMission;
+    public TextMeshProUGUI thirdMission;
+    public TextMeshProUGUI fourthMission;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
